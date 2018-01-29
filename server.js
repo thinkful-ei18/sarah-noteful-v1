7363281aa4a90763c9d9d96301cca0ddc2e78d18
ssheaks <sarah.sheaks@gmail.com>
2018-01-29 12:13:24 -0800

    add first get req to app

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 'use strict';
 
+const express = require('express')
+
 const data = require('./db/notes');
+
+const app = express();
+app.use(express.static('public'));
 
 console.log('hello world!');
 
 // INSERT EXPRESS APP CODE HERE...
+
+app.get('/v1/notes', (req, res) => {
+  res.json(data);
+});
+
+app.listen(8080, function () {
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
